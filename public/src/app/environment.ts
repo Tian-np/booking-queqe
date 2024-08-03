2151cd87f48c9d38fb19bef64da51f3c3a48bf9e
Tian-np <naphat4566@gmail.com>
2024-08-03 23:17:34 +0700

    Add firebase index.html

--- public/src/app/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyB3PkfXi_n1d3tWVGDUnJhiHygfxwEHnOY",
-    authDomain: "booking-queqe.firebaseapp.com",
-    projectId: "booking-queqe",
-    storageBucket: "booking-queqe.appspot.com",
-    messagingSenderId: "960564694351",
-    appId: "1:960564694351:web:09489a280e95d8e0e1493a",
-    measurementId: "G-K2MG3CETVW"
-  };
